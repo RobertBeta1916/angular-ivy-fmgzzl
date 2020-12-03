094c7bdf7b35a7d1b50e843f08c1e22e89c7f4bd
RobertBeta1916 <betagrobert@gmail.com>
2020-12-03 15:06:35 +0200

    7 files modified, 2 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, VERSION } from "@angular/core";
-
+ 
 @Component({
   selector: "my-app",
   templateUrl: "./app.component.html",
@@ -7,19 +7,19 @@ import { Component, VERSION } from "@angular/core";
 })
 export class AppComponent {
   name = "Angular 11";
-
+ 
   public userName = "Thibaut Bouldoire";
   public avgNoOfBooks = 9.5;
-
+ 
   getReadBooksAvg() {
     return Math.round(this.avgNoOfBooks);
   }
   getAverageColorState() {
     return this.avgNoOfBooks < 5;
-}
-
-public averageValueState = {
-"text-valid": this.avgNoOfBooks >= 5,
-"text-error": this.avgNoOfBooks < 5
-};
+  }
+ 
+  public averageValueState = {
+    "text-valid": this.avgNoOfBooks >= 5,
+    "text-error": this.avgNoOfBooks < 5
+  };
 }
--- src/app/filters/filters.component.ts
+++ src/app/filters/filters.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-
+ 
 @Component({
   selector: "app-filters",
   templateUrl: "./filters.component.html",
@@ -7,8 +7,8 @@ import { Component, OnInit } from "@angular/core";
 })
 export class FiltersComponent implements OnInit {
   public inputId = "";
-
+ 
   constructor() {}
-
+ 
   ngOnInit() {}
 }
